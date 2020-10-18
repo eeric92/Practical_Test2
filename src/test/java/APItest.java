0763dbd50d7b67d49f58111b0751d35248563b34
eric <eeric.92.es@gmail.com>
2020-10-18 12:15:47 +0200

    Added static final Strings

--- src/test/java/APItest.java
+++ src/test/java/APItest.java
@@ -11,16 +11,21 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class APItest {
 
+    private static final String URI = "https://api.github.com/repos";
+    private static final String METROLAB = "/metrolab";
+    private static final String SINGLEDATE = "/SingleDateAndTimePicker";
+    private static final String PATH_TO_JSON = "src/test/resources/response.json";
+
     @Test
     void TC001() throws IOException {
 
-        FileWriter fileWriter = new FileWriter("src/test/resources/response.json");
-        Response response = RestAssured.get("https://api.github.com/repos/metrolab/SingleDateAndTimePicker");
+        FileWriter fileWriter = new FileWriter(PATH_TO_JSON);
+        Response response = RestAssured.get(URI + METROLAB + SINGLEDATE);
 
         fileWriter.write(response.getBody().prettyPrint());
 
         given()
-                .get("https://api.github.com/repos/metrolab/SingleDateAndTimePicker")
+                .get(URI + METROLAB + SINGLEDATE)
                 .then()
                 .body("id", equalTo(132619461))
                 .body("node_id", equalTo("MDEwOlJlcG9zaXRvcnkxMzI2MTk0NjE="))
@@ -46,18 +51,16 @@ public class APItest {
                 .body("owner.type", equalTo("Organization"))
                 .body("owner.site_admin", equalTo(false))
                 .log().all();
-
     }
 
     @Test
     void TC002() {
 
         given()
-                .get("https://api.github.com/repos/metrolab/SingleDateAndTimePicker")
+                .get(URI + METROLAB + SINGLEDATE)
                 .then()
                 .body("owner.login", equalTo("seatcode")) //metrolab doesn't exists on response
                 .log().all();
-
     }
 
 }
